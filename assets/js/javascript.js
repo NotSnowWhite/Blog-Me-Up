3c140aa738422b00fc6ea55dec3296188bb073b1
NotSnowWhite <gflatch@att.net>
2024-04-02 15:52:38 -0500

    added javascript lines. began first function to ad event on click submit

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+const formSubmit = document.querySelector(button);
+
+formSubmit.addEventListener('click', function(event) {
+    event.preventDefault;
+    const Title = document.getElementById('title').value;
+    const user = document.getElementById('userName').value;
+
+} )

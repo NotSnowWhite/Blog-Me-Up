2333694985a251e26d3f82e444ae6d3d33241ee6
NotSnowWhite <gflatch@att.net>
2024-04-02 15:59:45 -0500

    added comments to java. added inputs to local storage. added reset to make form reset after submission

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,16 @@
+//pull the thing we want to manipulate(the submit button)
 const formSubmit = document.querySelector(button);
-
+//create a function to run on click of sub button that will save input to local storage
 formSubmit.addEventListener('click', function(event) {
     event.preventDefault;
     const Title = document.getElementById('title').value;
     const user = document.getElementById('userName').value;
+    const Cont = document.getElementById('content').value;
+//add to localStorage
+localStorage.setItem('Title', 'title');
+localStorage.setItem('user', 'userName');
+localStorage.setItem('content', 'content');
 
+//force reset form after submission
+formSubmit.reset();
 } )

63d9eeef46e4e3c68e88135531c435aaba02d2d3
NotSnowWhite <gflatch@att.net>
2024-04-02 21:13:05 -0500

    updated function for toggle button. prevented default of event button type so that it would stop flickering/reloading

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,19 +1,20 @@
 const btn = document.getElementById('back');
-const darkLight = document.getElementById('btn');
+const darkLight = document.getElementById('dark-light');
 let dark = true;
 
 btn.addEventListener('click', function() {
     window.history.back();
 });
 
-darkLight.addEventListener('click', function() {
+darkLight.addEventListener('click', function(event) {
+    event.preventDefault();
     if (dark) {
         document.querySelector('body').style.backgroundColor = 'purple';
         document.querySelector('body').style.color = 'pink';
-        dark = !dark;
     } else {
         document.querySelector('body').style.backgroundColor = 'pink';
         document.querySelector('body').style.color = 'purple';
-        dark = !dark;
     }
+    dark = !dark;
+
 })

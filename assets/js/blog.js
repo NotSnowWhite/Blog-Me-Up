24745e10972f260c03ee918de742d1dc4a815991
NotSnowWhite <gflatch@att.net>
2024-04-02 20:26:40 -0500

    added variables for the theme button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById('back');
+const darkLight = document.getElementById('btn');
+let dark = true;
 
 btn.addEventListener('click', function() {
     window.history.back();
 });
 
+darkLight.addEventListener('click', function() {
+    
+})

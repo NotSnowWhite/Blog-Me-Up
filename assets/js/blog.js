3ea5b6a6bc741a46ea3a597922e4c6f74aaa8ea3
NotSnowWhite <gflatch@att.net>
2024-04-02 21:35:08 -0500

    added comments and updated form to nav

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,12 +1,16 @@
+// variables I wanted to change
 const btn = document.getElementById('back');
 const darkLight = document.getElementById('dark-light');
 let dark = false;
 
+// function fires on click and goes back to previous web history page
 btn.addEventListener('click', function() {
     window.history.back();
 });
 
+// function fires on click and toggles color of background and text every click
 darkLight.addEventListener('click', function(event) {
+    // prevent default prevents the default action of the button (type:submit) from reloading
     event.preventDefault();
     if (dark) {
         document.querySelector('body').style.backgroundColor = 'purple';
@@ -18,3 +22,4 @@ darkLight.addEventListener('click', function(event) {
     dark = !dark;
 });
 
+// function for retrieving data in storage

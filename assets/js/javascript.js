08a148ffdb493a03660f918141248360faa49b35
NotSnowWhite <gflatch@att.net>
2024-04-02 22:39:05 -0500

    tried to get the stupid storage to work. doesn't. i no longer care so boo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,10 +6,16 @@ formSubmit.addEventListener('click', function(event) {
     const Title = document.getElementById('title').value;
     const user = document.getElementById('userName').value;
     const Content = document.getElementById('content').value;
+    const formData = {
+        title: Title,
+        user: user,
+        content: Content
+    };
+    const formDataString = JSON.stringify(formData);
+
 //add to localStorage
-localStorage.setItem('title', Title);
-localStorage.setItem('user', user);
-localStorage.setItem('content', Content);
+localStorage.setItem('formData', formDataString);
+
 
 //force reset form after submission
 formSubmit.reset();

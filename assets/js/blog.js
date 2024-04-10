8b13f361e77a1cb19497be1b209acf9a623b961a
NotSnowWhite <gflatch@att.net>
2024-04-10 10:26:13 -0500

    removed action from form and added direction at end of submit button to redirect page upon submit. fixed prevent default method in submit button. changed stringify to data from formData since formdata was saved in variable data. removed elements inside sections of blog.html and added for loop to loop through the array and create elements for each object value; appended to page. removed if statement in favor of for loop and added posts id to html to pull a container to hold data array data.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,12 +4,12 @@ const darkLight = document.getElementById('dark-light');
 let dark = false;
 
 // function fires on click and goes back to previous web history page
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     window.history.back();
 });
 
 // function fires on click and toggles color of background and text every click
-darkLight.addEventListener('click', function(event) {
+darkLight.addEventListener('click', function (event) {
     // prevent default prevents the default action of the button (type:submit) from reloading
     event.preventDefault();
     if (dark) {
@@ -22,19 +22,31 @@ darkLight.addEventListener('click', function(event) {
     dark = !dark;
 });
 
-// retrieving data in storage
-const data = JSON.parse(localStorage.getItem('formData'));
-
-
+// retrieving data in Localstorage
+let data = JSON.parse(localStorage.getItem('formData')) || [];
 // pulling a place to put data
-const titleEl = document.getElementById('title');
-const userEl = document.getElementById('user');
-const contentEl = document.getElementById('content');
+// const titleEl = document.getElementById('title');
+// const userEl = document.getElementById('user');
+// const contentEl = document.getElementById('content');
+const postsEl = document.getElementById('posts');
 
-// display data
-if (data) {
-titleEl.textContent = data.title;
-userEl.textContent = data.user;
-contentEl.textContent = data.content;
+//  loop through the data array and create an element for each entry/ display data
+for (let index = 0; index < data.length; index++) {
+    const entry = data[index];
+    const h2El = document.createElement('h2');
+    h2El.textContent = entry.title;
+    const pEl1 = document.createElement('p');
+    pEl1.textContent = entry.user;
+    const pEl2 = document.createElement('p');
+    pEl2.textContent = entry.content;
+    postsEl.append(h2El, pEl1, pEl2);
 
 }
+
+// display data
+// if (data) {
+// titleEl.textContent = data.title;
+// userEl.textContent = data.user;
+// contentEl.textContent = data.content;
+
+// }
